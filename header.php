a0d7c0e293d7696bbb5399088c4992878b6a2066
kevinquidel <kevinquidel_12@hotmail.es>
2023-05-22 05:32:50 -0500

    git add . git commit -m "commit" git push origin main

--- header.php
+++
@@ -1,8 +0,0 @@
-<h1>Xavaya.com - Página oficial de Xavaya</h1>
-<nav>
-  <ul>
-    <li><a href="#">Inicio</a></li>
-    <li><a href="#">Acerca de</a></li>
-    <li><a href="#">Contacto</a></li>
-  </ul>
-</nav>
--- search-form.php
+++
@@ -1,4 +0,0 @@
-<form action="resultados.php" method="get">
-  <input type="text" name="search" placeholder="Buscar...">
-  <input type="submit" value="Buscar">
-</form>
