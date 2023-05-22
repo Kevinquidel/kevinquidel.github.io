3fd8e118643fc42f6c51005dd18e0db3ce5414a6
kevinquidel <kevinquidel_12@hotmail.es>
2023-05-22 04:46:25 -0500

    git add . git commit -m "renamed:    index.html -> index.php" git push origin main

--- index.php
+++ index.php
@@ -88,6 +88,33 @@
       cursor: pointer;
     }
     
+    /* Estilos del resultado de búsqueda */
+    .search-results {
+      margin-top: 20px;
+    }
+    
+    .search-results p {
+      font-weight: bold;
+    }
+    
+    .search-results ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    
+    .search-results li {
+      margin-bottom: 10px;
+    }
+    
+    .search-results li a {
+      color: #333333;
+      text-decoration: none;
+    }
+    
+    .search-results li a:hover {
+      color: #ff6600;
+    }
+    
     /* Estilos del pie de página */
     footer {
       background-color: #333333;
@@ -118,6 +145,61 @@
       </form>
     </div>
   
+    <?php
+    if (isset($_GET['search']) && !empty($_GET['search'])) {
+      $searchQuery = $_GET['search'];
+
+      // Array de resultados simulados
+      $results = array(
+        array(
+          "title" => "Título del resultado 1",
+          "url" => "https://www.example.com/result1",
+          "description" => "Descripción del resultado 1"
+        ),
+        array(
+          "title" => "Título del resultado 2",
+          "url" => "https://www.example.com/result2",
+          "description" => "Descripción del resultado 2"
+        ),
+        array(
+          "title" => "Título del resultado 3",
+          "url" => "https://www.example.com/result3",
+          "description" => "Descripción del resultado 3"
+        )
+      );
+
+      // Filtrar los resultados que coincidan con la consulta de búsqueda
+      $filteredResults = array();
+      foreach ($results as $result) {
+        if (strcasecmp($result['title'], $searchQuery) === 0 || strcasecmp($result['description'], $searchQuery) === 0) {
+          $filteredResults[] = $result;
+        }
+      }
+
+      // Mostrar los resultados
+      if (count($filteredResults) > 0) {
+        echo "<div class='search-results'>";
+        echo "<p>Se encontraron " . count($filteredResults) . " resultados para '$searchQuery':</p>";
+        echo "<ul>";
+
+        foreach ($filteredResults as $result) {
+          echo "<li><a href='" . $result['url'] . "'>" . $result['title'] . "</a> - " . $result['description'] . "</li>";
+        }
+
+        echo "</ul>";
+        echo "</div>";
+      } else {
+        echo "<div class='search-results'>";
+        echo "<p>No se encontraron resultados para '$searchQuery'.</p>";
+        echo "</div>";
+      }
+    } else {
+      echo "<div class='search-results'>";
+      echo "<p>No se especificó ninguna consulta de búsqueda.</p>";
+      echo "</div>";
+    }
+    ?>
+    
     <h2>Contenido principal</h2>
     <p>Aquí puedes agregar tu contenido principal.</p>
   </main>
